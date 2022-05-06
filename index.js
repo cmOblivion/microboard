28664c5c6881ecfe2e17e86bf28222c6d68dd078
cm <chenzihandawang@163.com>
2022-05-06 19:29:31 +0800

    update

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const server = require('./server'),
 
 const options = JSON.parse(readFileSync('./config.json').toString());
 
+if (process.env.PORT) {
+	options.port = process.env.PORT;
+}
+
 const sv = server.start(options);
